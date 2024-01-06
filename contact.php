f1a0012218775c15f2d8abf14305ce7df2f652ba
rajeshkumar-0308 <rajeshsaravanan2004@gmail.com>
2024-01-06 12:33:46 +0530

    1st commit

--- contact.php
+++ contact.php
@@ -1,13 +1,5 @@
 <?php
 
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'rajeshsaravanan2004@gamil.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -33,17 +25,6 @@
     'port' => '2525'
   );
 */
-    Email.send({
-      Host => "smtp.elasticemail.com",
-      Username => "rajeshsaravanan2004@gmail.com",
-      Password => "052E0DE92AA305349AF8A3E028224DF7DE92",
-      To => 'rajeshsaravanan2004@gmail.com',
-      From => "rajeshsaravanan2004@gmail.com",
-      Subject => "This is the subject",
-      Body => "And this is the body"
-  }).then(
-    message => alert(message)
-  );
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
